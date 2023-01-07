7d6471f931bbe806b59b348f5668f4d709d16461
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-01-07 17:34:41 +0530

    Remove console.log statements

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -8,47 +8,35 @@ const App = () => {
   const [countriesData, setCountriesData] = useState([]);
   const [countriesSearched, setCountriesSearched] = useState([]);
   useEffect(() => {
-    console.log("useeffect 1 is called");
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountriesData(response.data);
-      console.log(
-        "data from restcountries is fetched and countriesData is changed"
-      );
     });
   }, []);
   useEffect(() => {
-    console.log("useeffect 2 is called");
     let countriesFiltered = countriesData.filter((country) => {
       return searchInput
         ? country.name.official.toLowerCase().includes(searchInput)
         : false;
     });
-    console.log("countries are filtered ", countriesFiltered);
     if (countriesFiltered.length == 1) {
       axios
         .get(
           `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${countriesFiltered[0].latlng.join()}`
         )
         .then((response) => {
-          console.log(
-            "weather data is fetched and countriesSearched is changed"
-          );
           countriesFiltered[0].weather = response.data;
           setCountriesSearched(countriesFiltered);
         });
     } else {
       setCountriesSearched(countriesFiltered);
-      console.log("countriesSearched is changed");
     }
   }, [searchInput]);
-  console.log("App rendered!");
   return (
     <div>
       <Search
         searchValue={searchInput}
         handleSearchChange={(event) => {
           setSearchInput(event.target.value);
-          console.log("search input changed");
         }}
       />
       <Country countries={countriesSearched} />
