2946de287ad2b4c107552be332a57cc27ed6f375
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-02-28 14:53:49 +0530

    Deploy frontend to backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,6 +8,7 @@ morgan.token("data", (request, response) => {
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :data",
