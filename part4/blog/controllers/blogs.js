31c7a87b4332200fade24d49264c9c2ba44b3dc4
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-03-13 19:58:47 +0530

    refactor get route handler

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,25 +3,20 @@ const Blog = require("../models/blogs.js");
 
 const blogsRouter = require("express").Router();
 blogsRouter.get("/", async (request, response, next) => {
-  try {
-    const blogsFound = await Blog.find({});
-    if (blogsFound) {
-      logger.info("blogs fetched successfully!");
-      response.json(blogsFound);
-    }
-  } catch (error) {
-    next(error);
+  const blogs = await Blog.find({});
+  if (blogs) {
+    logger.info("blogs fetched successfully!");
+    response.json(blogs);
   }
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  try {
-    const blog = new Blog(request.body);
-    const blogSaved = await blog.save();
-    reponse.status(201).json(blogSaved);
-  } catch (error) {
-    next(error);
-  }
+blogsRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body);
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
-
 module.exports = blogsRouter;
