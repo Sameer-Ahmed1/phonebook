152af58c26ba593807fabf952c4681e4bf4f94e8
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-03-14 17:07:34 +0530

    add userRoute

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs.js");
+const usersRouter = require("./controllers/users.js");
 const logger = require("./utils/logger.js");
 const config = require("./utils/config.js");
 const middleware = require("./utils/middleware.js");
@@ -21,6 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 // app.use(morgan.postLogger); not using during  testing
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
