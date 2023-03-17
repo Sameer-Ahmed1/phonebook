60216b2b3652be2b9a5891a141b5b830bc7ead1d
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-03-17 21:39:28 +0530

    add 401 unath

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -14,7 +14,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "MongoServerError") {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
-    return response.status(400).json({ error: error.message });
+    return response.status(401).json({ error: error.message });
   }
   next(error);
 };
